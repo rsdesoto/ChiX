2ac76371bda92e16b0029a858aaef58d109e2d1a
jeast <eastwoodj000@gmail.com>
2018-11-27 13:15:57 -0600

    added link to change the page on for submission and changed api access order

--- assets/javascript/searchResults.js
+++ assets/javascript/searchResults.js
@@ -22,24 +22,24 @@ database.ref('/location').once('value', function(snapshot) {
     address = snapshot.val().address;//the last line of the search results html happens before this so an error occurs
     $.ajax({
         async: false,
-        url: `https://api.foursquare.com/v2/venues/search?client_id=XLARRNIFOXVD2CYYWZTPLXOXPI3BFBECOJTZEVZAI0OCO01S&client_secret=TNAAYAFVDDSPVDK1RTGIW2VPZTBKCOAVYVXSYEBBU2MXF015&v=20180323&query=${query}&limit=30&near=${address}`,
-        method: 'GET'
-    }).then(function(response) {
-        for (let i = 0; i < 20; i++) {
-            resultsName.push(response.response.venues[i].name);
-            resultsLat.push(response.response.venues[i].location.lat);
-            resultsLng.push(response.response.venues[i].location.lng);
-        }
-    });
-    $.ajax({
-        async: false,
         url: `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=AIzaSyA_8m3vV01mZAdSvesbW3G2rkoHLW4WP2s`,
         method: 'GET',
     }).then(function(response) {
         searchLat = response.results[0].geometry.location.lat;
         searchLng = response.results[0].geometry.location.lng;
-        $('body').append($('<script id="appendedScript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCYgcY03FvjLBqaWUGRt-PyD8soS3aAvyA&callback=initMap"type="text/javascript"></script>'));
-        //https://www.html5rocks.com/en/tutorials/speed/script-loading/ idea for the above statement
+        $.ajax({
+            async: false,
+            url: `https://api.foursquare.com/v2/venues/search?client_id=XLARRNIFOXVD2CYYWZTPLXOXPI3BFBECOJTZEVZAI0OCO01S&client_secret=TNAAYAFVDDSPVDK1RTGIW2VPZTBKCOAVYVXSYEBBU2MXF015&v=20180323&query=${query}&limit=30&ll=${searchLat},${searchLng}`,
+            method: 'GET'
+        }).then(function(response) {
+            for (let i = 0; i < 20; i++) {
+                resultsName.push(response.response.venues[i].name);
+                resultsLat.push(response.response.venues[i].location.lat);
+                resultsLng.push(response.response.venues[i].location.lng);
+            }
+            $('body').append($('<script id="appendedScript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCYgcY03FvjLBqaWUGRt-PyD8soS3aAvyA&callback=initMap"type="text/javascript"></script>'));
+            //https://www.html5rocks.com/en/tutorials/speed/script-loading/ idea for the above statement
+        });
     });
 });
 
@@ -47,24 +47,25 @@ $(document).on('click', '#searchLocation', function() {
     event.preventDefault();
     address = $('#location').val();
     $.ajax({
-        url: `https://api.foursquare.com/v2/venues/search?client_id=XLARRNIFOXVD2CYYWZTPLXOXPI3BFBECOJTZEVZAI0OCO01S&client_secret=TNAAYAFVDDSPVDK1RTGIW2VPZTBKCOAVYVXSYEBBU2MXF015&v=20180323&query=${query}&limit=30&near=${address}`,
-        method: 'GET'
-    }).then(function(response) {
-        for (let i = 0; i < 20; i++) {
-            resultsName.push(response.response.venues[i].name);
-            resultsLat.push(response.response.venues[i].location.lat);
-            resultsLng.push(response.response.venues[i].location.lng);
-        }
-    });
-    $.ajax({
         async: false,
         url: `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=AIzaSyA_8m3vV01mZAdSvesbW3G2rkoHLW4WP2s`,
         method: 'GET',
     }).then(function(response) {
         searchLat = response.results[0].geometry.location.lat;
         searchLng = response.results[0].geometry.location.lng;
-        $('#appendedScript').remove();
-        $('body').append($('<script id="appendedScript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCYgcY03FvjLBqaWUGRt-PyD8soS3aAvyA&callback=initMap"type="text/javascript"></script>'));
+        $.ajax({
+            async: false,
+            url: `https://api.foursquare.com/v2/venues/search?client_id=XLARRNIFOXVD2CYYWZTPLXOXPI3BFBECOJTZEVZAI0OCO01S&client_secret=TNAAYAFVDDSPVDK1RTGIW2VPZTBKCOAVYVXSYEBBU2MXF015&v=20180323&query=${query}&limit=30&ll=${searchLat},${searchLng}`,
+            method: 'GET'
+        }).then(function(response) {
+            for (let i = 0; i < 20; i++) {
+                resultsName.push(response.response.venues[i].name);
+                resultsLat.push(response.response.venues[i].location.lat);
+                resultsLng.push(response.response.venues[i].location.lng);
+            }
+            $('body').append($('<script id="appendedScript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCYgcY03FvjLBqaWUGRt-PyD8soS3aAvyA&callback=initMap"type="text/javascript"></script>'));
+            //https://www.html5rocks.com/en/tutorials/speed/script-loading/ idea for the above statement
+        });
     });
   });
 
@@ -81,7 +82,7 @@ function initMap() {   //this functional has to match final call
     streetViewControl: true,
     fullScreenControl: true
   });
-  for (let i = 0; i < 20; i++) {
+  for (let i = resultsLat.length - 20; i < resultsLat.length; i++) {
         let latlng = {lat: resultsLat[i], lng: resultsLng[i]}
         let marker = new google.maps.Marker({
         position: latlng,
@@ -89,6 +90,6 @@ function initMap() {   //this functional has to match final call
         map: map
         });
         marker.setMap(map);
-        $('ol').append($(`<li>${resultsName[i]}</li>`))
+        $('ol').append($(`<li>${resultsName[i]}</li>`));
     }
 };
